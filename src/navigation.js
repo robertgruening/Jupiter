596085ce6d264444beefd2db21005f9991291073
Robert Grüning <r.gruening@web.de>
2017-03-20 22:59:36 +0100

    Einheitliche Strukturierung begonnen, Navigation eingebunden, HTML, CSS und Javascript begonnen zu trennen

---
+++ src/navigation.js
@@ -0,0 +1,24 @@
+function GetDatabaseName() {
+	return "jupiter";
+}
+
+function GetPageUrl(pageName) {
+	return "http://localhost:5984/" + GetDatabaseName() + "/app/" + pageName + ".html";
+}
+
+function LoadNavigation(activePageName) {
+	var navigationElements = new Array();
+	navigationElements.push("Elemente");
+	navigationElements.push("Element");
+	navigationElements.push("Formular");
+	navigationElements.push("Import");
+	
+	$("#navigation").append("<ul></ul>");
+	
+	for (var i = 0; i < navigationElements.length; i++)
+	{	
+		$("#navigation ul").append("<li><a href='" + GetPageUrl(navigationElements[i]) + "' title=" + navigationElements[i] + ">" + navigationElements[i] + "</a></li>");
+	}
+	
+	$("#navigation ul li a[title='" + activePageName + "']").addClass("active");
+}
